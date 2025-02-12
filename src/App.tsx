426fb257b861e1084d3e7f903528f3be3e342290
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-02-12 23:00:02 +0100

    Add single message conversation to UI
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,43 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
+import { createResource, createSignal, Match, Show, Switch } from "solid-js";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name: name() }));
+  async function sendMsg(userMsg: string): Promise<string> {
+    return await invoke("prompt", { userMsg });
   }
+
+  const [input, setInput] = createSignal<HTMLInputElement>();
+  const [userMsg, setUserMsg] = createSignal("");
+  const [assistantMsg] = createResource(userMsg, sendMsg)
 
   return (
     <main class="container">
-      <h1>Welcome to Tauri + Solid</h1>
+      <p>User: {userMsg()}</p>
+      <Show when={assistantMsg.loading}>
+        <p>Loading...</p>
+      </Show>
+      <Switch>
+        <Match when={assistantMsg.error}>
+          <span>Error: {assistantMsg.error}</span>
+        </Match>
+        <Match when={assistantMsg()}>
+          <p>Assistant: {assistantMsg()}</p>
+        </Match>
+      </Switch>
 
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <form
-        class="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+      <input
+        id="chat-input"
+        ref={setInput}
+        placeholder="Enter a message..."
+      />
+      <button
+        onClick={() => {
+          setUserMsg(input()?.value ?? "");
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg()}</p>
+        Send
+      </button>
     </main>
   );
 }
