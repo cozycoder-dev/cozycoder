c0ff5bbe0ef0d235e2de9ab690d5042d4e6a8464
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-03-12 15:42:59 +0100

    Downgrade to Tailwind 3
    Tailwind 3 standalone binary will be downloaded automatically by Trunk.
    This makes the setup in CI and dev much easier.
    
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- crates/app/src/app.rs
+++ crates/app/src/app.rs
@@ -5,6 +5,7 @@ pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
         <button on:click=move |_| set_count.set(3)>"Click me: " {count}</button>
         <p>"Double count: " {move || count.get() * 2}</p>
     }
