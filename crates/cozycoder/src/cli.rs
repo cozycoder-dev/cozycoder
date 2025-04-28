cceb1a769576cae3227e1d98c037bc0a870b2066
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-04-28 23:18:00 +0200

    Add hello world MCP server
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- crates/cozycoder/src/cli.rs
+++ crates/cozycoder/src/cli.rs
@@ -1,9 +1,26 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use cozycoder_mcp_server::run_mcp_server;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None,  arg_required_else_help = true)]
-struct Cli {}
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
-pub fn run() {
-    let _cli = Cli::parse();
+#[derive(Subcommand)]
+enum Commands {
+    Mcp,
+}
+
+pub async fn run() {
+    let cli = Cli::parse();
+
+    if let Some(command) = &cli.command {
+        match command {
+            Commands::Mcp => {
+                let _ = run_mcp_server().await;
+            }
+        }
+    }
 }
---
+++ crates/mcp-server/src/lib.rs
@@ -0,0 +1,55 @@
+use handler::CozyCoderServerHandler;
+use rust_mcp_schema::{
+    Implementation, InitializeResult, LATEST_PROTOCOL_VERSION, ServerCapabilities,
+    ServerCapabilitiesTools,
+};
+use rust_mcp_sdk::{
+    McpServer, StdioTransport, TransportError, TransportOptions, error::McpSdkError,
+    mcp_server::server_runtime,
+};
+
+pub(crate) mod handler;
+
+#[derive(Debug, thiserror::Error)]
+pub enum McpError {
+    #[error("Server error: {0}")]
+    ServerError(String),
+}
+
+impl From<McpSdkError> for McpError {
+    fn from(err: McpSdkError) -> Self {
+        McpError::ServerError(format!("SDK error - {}", err))
+    }
+}
+
+impl From<TransportError> for McpError {
+    fn from(err: TransportError) -> Self {
+        McpError::ServerError(format!("Transport error - {}", err))
+    }
+}
+
+pub fn server_details() -> InitializeResult {
+    InitializeResult {
+        server_info: Implementation {
+            name: "cozycoder".to_string(),
+            version: env!("CARGO_PKG_VERSION").to_string(),
+        },
+        capabilities: ServerCapabilities {
+            experimental: None,
+            logging: None,
+            prompts: None,
+            resources: None,
+            tools: Some(ServerCapabilitiesTools { list_changed: None }),
+        },
+        instructions: None,
+        meta: None,
+        protocol_version: LATEST_PROTOCOL_VERSION.to_string(),
+    }
+}
+
+pub async fn run_mcp_server() -> Result<(), McpError> {
+    let transport = StdioTransport::new(TransportOptions::default())?;
+    let handler = CozyCoderServerHandler {};
+    let server = server_runtime::create_server(server_details(), transport, handler);
+    server.start().await.map_err(McpError::from)
+}
