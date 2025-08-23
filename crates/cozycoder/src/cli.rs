48cf1dffa4aaa3cf8b60b200885118cc2b8c92f2
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-08-23 16:18:41 +0200

    Remove old Rust project
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- crates/cozycoder/src/cli.rs
+++
@@ -1,9 +0,0 @@
-use clap::Parser;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None,  arg_required_else_help = true)]
-struct Cli {}
-
-pub fn run() {
-    let _cli = Cli::parse();
-}
