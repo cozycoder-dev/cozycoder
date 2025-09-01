68d5cda942ebcbfdb27ce114e2bcd2c6aa80fc26
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-09-01 15:56:08 +0200

    Add project entity and API
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

---
+++ lib/cozycoder/projects/project.ex
@@ -0,0 +1,20 @@
+defmodule CozyCoder.Projects.Project do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "projects" do
+    field :name, :string
+    field :git_url, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(project, attrs) do
+    project
+    |> cast(attrs, [:name, :git_url])
+    |> validate_required([:name, :git_url])
+  end
+end
---
+++ test/support/fixtures/projects_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule CozyCoder.ProjectsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `CozyCoder.Projects` context.
+  """
+
+  @doc """
+  Generate a project.
+  """
+  def project_fixture(attrs \\ %{}) do
+    {:ok, project} =
+      attrs
+      |> Enum.into(%{
+        git_url: "some git_url",
+        name: "some name"
+      })
+      |> CozyCoder.Projects.create_project()
+
+    project
+  end
+end
